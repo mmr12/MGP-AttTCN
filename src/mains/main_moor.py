541c1460dd44309496a2b64771b8c4faf58eca66
margheritarosnati <mmr12@ic.ac.uk>
2021-03-11 16:02:38 +0000

    added moor model

---
+++ src/mains/main_moor.py
@@ -0,0 +1,196 @@
+import os
+import sys
+from datetime import datetime
+import numpy as np
+import tensorflow as tf
+from argparse import ArgumentParser
+import pickle
+cwd = os.path.dirname(os.path.abspath(__file__))
+head = os.path.abspath(os.path.join(cwd, os.pardir, os.pardir))
+sys.path.append(head)
+from src.utils.debug import t_print
+from src.data_loader.loader import DataGenerator
+from src.models.GP_TCN import GP_TCN
+from src.trainers.trainer import Trainer
+
+
+def main(
+        # data
+        max_no_dtpts,
+        min_no_dtpts,
+        time_window,
+        n_features,
+        n_stat_features,
+        features,
+        late_patients_only,
+        horizon0,
+        # MGP
+        no_mc_samples,
+        kernel_choice,
+        # TCN
+        num_layers,
+        kernel_size,
+        stride,
+        DO,
+        L2reg,
+        sigmoid_beta,
+        # training
+        learning_rate,
+        batch_size,
+        num_epochs,):
+    # generate save path
+    logdir = os.path.join( "logs/", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    if not os.path.isdir(logdir):
+        os.mkdir(logdir)
+    Dict = {
+        # data
+        "max_no_dtpts":max_no_dtpts,
+        "min_no_dtpts":min_no_dtpts,
+        "time_window": time_window,
+        "n_features": n_features,
+        "n_stat_features": n_stat_features,
+        "features": features,
+        "late_patients_only": late_patients_only,
+        "horizon0": horizon0,
+        # MGP
+        "no_mc_samples": no_mc_samples,
+        "kernel_choice": kernel_choice,
+        # TCN
+        "num_layers": num_layers,
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "DO": DO,
+        "L2reg": L2reg,
+        "sigmoid_beta": sigmoid_beta,
+        # training
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "num_epochs": num_epochs,
+    }
+    with open(os.path.join(logdir, 'hyperparam.pkl'), "wb") as f:
+        pickle.dump(Dict, f)
+
+    summary_writers = {'train': tf.summary.create_file_writer(os.path.join(logdir, 'train')),
+                       'val': tf.summary.create_file_writer(os.path.join(logdir, 'val')),
+                       'val_hz': tf.summary.create_file_writer(os.path.join(logdir, 'val_hz'))
+                       }
+    t_print("nu_layers: {}\tlr: {}\tMC samples :{}\tDO :{}\tL2 :{}\t kernel:{}".format(num_layers, learning_rate, no_mc_samples, DO[0], L2reg[0], kernel_size))
+    # Load data
+    data = DataGenerator(no_mc_samples=no_mc_samples,
+                         max_no_dtpts=max_no_dtpts,
+                         min_no_dtpts=min_no_dtpts,
+                         batch_size=batch_size,
+                         fast_load=False,
+                         to_save=True,
+                         debug=True,
+                         fixed_idx_per_class=False,
+                         features=features)
+
+    t_print("main - generate model and optimiser")
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    global_step = tf.Variable(0)
+
+    # Load model
+    model = GP_TCN(time_window,
+                     no_mc_samples,
+                     n_features,
+                     n_stat_features,
+                     kernel=kernel_choice,
+                     L2reg=L2reg,
+                     DO=DO,
+                     num_layers=num_layers,
+                     kernel_size=kernel_size,
+                     stride=stride,
+                     sigmoid_beta=sigmoid_beta)
+
+    # Initialise trainer
+    trainer = Trainer(model=model,
+                      data=data,
+                      num_epochs=num_epochs,
+                      batch_size=batch_size,
+                      optimizer=optimizer,
+                      global_step=global_step,
+                      summary_writers=summary_writers,
+                      log_path=logdir,
+                      train_only=False,
+                      notebook_friendly=False,
+                      eval_every=20,
+                      late_patients_only=late_patients_only,
+                      horizon0=horizon0,)
+
+    # train model
+    trainer.run()
+
+
+if __name__=="__main__":
+    #tf.random.set_seed(1237)
+    #np.random.seed(1237)
+    # data
+    max_no_dtpts = 250  # chopping 4.6% of data at 250
+    min_no_dtpts = 40  # helping with covariance singularity
+    time_window = 25  # fixed
+    n_features = 24  # old data: 44
+    n_stat_features = 8  # old data: 35
+    late_patients_only = False
+    horizon0 = False
+
+    # MGP
+    kernel_choice = 'OU'
+
+    # TCN
+    stride = 1
+    DO = [0.01] * 10
+    sigmoid_beta = True
+
+    # training
+    batch_size = 128
+    num_epochs = 100
+
+
+    parser = ArgumentParser()
+    parser.add_argument('--learning_rate',
+                        default=np.random.uniform(10e-6, high=10e-4, size=None),
+                        type=float)
+    parser.add_argument('--no_mc_samples',
+                        default=np.random.randint(8, high=20, size=None, dtype='l'),
+                        type=int)
+    parser.add_argument('--L2reg', default=np.random.randint(-5, high=8, size=None, dtype='l'), type=float)
+    parser.add_argument('--kernel_size', default=np.random.randint(2, high=6, size=None, dtype='l'), type=int)
+    parser.add_argument('--num_layers', default=np.random.randint(2, high=8, size=None, dtype='l'), type=int)
+    parser.add_argument('--seed', default=np.random.randint(1, high=9999, size=None, dtype='l'), type=int)
+    parser.add_argument('--features', default='rosnati', type=str)
+    args = parser.parse_args()
+    learning_rate = args.learning_rate
+    no_mc_samples = args.no_mc_samples
+    kernel_size = (args.kernel_size,)
+    num_layers = args.num_layers
+    seed = args.seed
+    features =args.features
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+    L2reg = [10**float(args.L2reg)] * 10
+
+    main(
+        # data
+        max_no_dtpts,
+        min_no_dtpts,
+        time_window,
+        n_features,
+        n_stat_features,
+        features,
+        late_patients_only,
+        horizon0,
+        # MGP
+        no_mc_samples,
+        kernel_choice,
+        # TCN
+        num_layers,
+        kernel_size,
+        stride,
+        DO,
+        L2reg,
+        sigmoid_beta,
+        # training
+        learning_rate,
+        batch_size,
+        num_epochs)
---
+++ src/models/GP_TCN.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import tensorflow as tf
+import numpy as np
+# appending head path
+cwd = os.path.dirname(os.path.abspath(__file__))
+head = os.path.abspath(os.path.join(cwd, os.pardir, os.pardir, os.pardir))
+sys.path.append(head)
+from src.models.GP import MultiKernelMGPLayer
+from src.models.TCN import make_model
+
+
+class GP_TCN:
+    def __init__(self,
+                 time_window,
+                 n_mc_samples,
+                 n_features,
+                 n_stat_features,
+                 kernel='OU',
+                 len_mode='avg',
+                 len_trainable=True,
+                 log_noise_mean=-2,
+                 log_noise_std=0.1,
+                 log_length_mean=1,
+                 log_length_std=0.1,
+                 method_name='chol',
+                 add_diag=0.001,
+                 L2reg=None,
+                 DO=None,
+                 save_path=head,
+                 num_layers=4,
+                 kernel_size=2,
+                 stride=1,
+                 sigmoid_beta=False,
+                 moor_data=False
+                 ):
+        # a few variables to be used later
+        self.tw = time_window
+        self.s_feat = n_stat_features
+        self.samp = n_mc_samples
+
+        # the model
+        self.GP = MultiKernelMGPLayer(time_window=time_window,
+                                      n_mc_samples=n_mc_samples,
+                                      n_features=n_features,
+                                      log_noise_mean=log_noise_mean,
+                                      log_noise_std=log_noise_std,
+                                      method_name=method_name,
+                                      add_diag=add_diag,
+                                      save_path=save_path)
+
+        self.TCN = make_model(time_window=time_window,
+                                  no_channels=n_features + n_stat_features,
+                                  L2reg=L2reg,
+                                  DO=DO,
+                                  num_layers=num_layers,
+                                  kernel_size=kernel_size,
+                                  stride=stride,
+                                  add_classification_layer=True)
+
+        self.trainable_variables = self.GP.trainable_variables + \
+                                   self.TCN.trainable_variables
+        self.n_GP_var = len(self.GP.trainable_variables)
+
+    def __call__(self, inputs):
+        self.GP_out = self.GP(inputs[:-1])
+        stat_matching_shape = \
+            tf.concat([  # step III: concatenate all patients i
+                # step II: tile patient info for each MC sample
+                tf.tile(
+
+                    # step I: for pat i, repeat feat data for each time step
+                    tf.concat([tf.reshape(inputs[-1][i], [1, 1, self.s_feat]) for _ in range(self.tw)], axis=1)
+                    , [self.samp, 1, 1])
+                for i in range(inputs[-1].shape[0])], axis=0)
+
+        self.TCN_input = tf.concat([self.GP_out, stat_matching_shape], -1)
+
+        return self.TCN(self.TCN_input)
+
+    def get_weights(self):
+        self.trainable_variables = self.GP.trainable_variables + \
+                                   self.TCN.trainable_variables
+        return self.trainable_variables
+
+    def set_weights(self, weights):
+        if not isinstance(weights[0], np.ndarray):
+            weights = [weights[i].numpy() for i in range(len(weights))]
+        self.GP.set_weights(weights[:self.n_GP_var])
+        self.TCN.set_weights(weights[self.n_GP_var:])
